3b165067380eb52fc9ef683216ebca446691241c
zk229 <zkirsche01@gmail.com>
2022-03-06 20:55:31 -0500

    initial setup for main script

---
+++ index.js
@@ -0,0 +1,6 @@
+// Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Intern = require("./lib/Intern.js");
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
