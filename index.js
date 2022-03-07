6b5dca8e30258a016051b055ab5f0dec7185be64
zk229 <zkirsche01@gmail.com>
2022-03-06 21:18:19 -0500

    outline index.js

--- index.js
+++ index.js
@@ -6,5 +6,41 @@ const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const questions = require("./lib/questions.js");
 
-// Questions for Inquirer module
-console.log(questions.askNext);
+// Global variables
+const teamList = [];
+
+function askEngineer() {
+    
+};
+
+function askIntern() {
+
+};
+
+function finalize() {
+
+};
+
+function menu() {
+    inquirer.prompt(questions.askNext).then(answers => {
+        switch(answers.next) {
+            case "Engineer":
+                askEngineer();
+                break;
+            case "Intern":
+                askIntern();
+                break;
+            case "Finalize":
+                finalize();
+        }
+    });
+};
+
+function init() {
+    inquirer.prompt(questions.managerQ).then(answers => {
+        teamList.push(new Manager(answers.name, answers.email, answers.id, answers.room));
+        menu();
+    });
+};
+
+init();
